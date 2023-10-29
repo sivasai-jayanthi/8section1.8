24989d1541d952787022bd64860755485cea4747
sivasai-jayanthi <sai89938@gmail.com>
2023-10-30 00:31:49 +0530

    Add project files.

---
+++ 8section1.8/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _8section1._8
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			runApp();
+			Console.ReadLine();
+		}
+
+		public static void runApp()
+		{
+			DoubleLinkedList list = new DoubleLinkedList();
+			Node n = new Node();
+			n.Data = "root";
+			list.add(n);
+			for (int i = 0; i < 10; i++)
+			{
+				Node node = new Node();
+				node.Data = Convert.ToString(i);
+				list.add(node);
+			}
+
+			Console.WriteLine("length of singly linked list=" + list.getLengthOfList());
+			Console.WriteLine("Traversing forward..");
+			n = list.getRoot();
+			while (n != null)
+			{
+				Console.WriteLine(n.Data);
+				n = list.getNext();
+			}
+
+
+			Console.WriteLine("Traversing backwards..");
+			n = list.getPrev();
+			while (n != null)
+			{
+				Console.WriteLine(n.Data);
+				n = list.getPrev();
+			}
+
+		}
+	}
+}
---
+++ 8section1.8/node.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _8section1._8
+{
+	public class Node
+	{
+		private string data;
+		private Node next = null;
+		private Node prev = null;
+
+		public string Data
+		{
+			get { return this.data; }
+			set { this.data = value; }
+		}
+
+		public Node Next
+		{
+			get { return this.next; }
+			set { this.next = value; }
+		}
+
+		public Node Prev
+		{
+			get { return this.prev; }
+			set { this.prev = value; }
+		}
+	}
+}
